0bb23765cd235f36559e1e9f4bcd85a64ae11cda
tomcousin <tom.cousin@epitech.eu>
2021-02-26 17:31:27 +0900

    [+] Add basic Constants to the project

---
+++ EggWarriors/EggWarriors/Constants/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  EggWarriors
+//
+//  Created by Tom Cousin on 26/02/2021.
+//
+
+import Foundation
+import SpriteKit
+
+enum Constants {
+
+    enum UI {
+
+        enum Color {
+
+            static let BaseColor: SKColor = SKColor.black
+        }
+
+        enum Rectangle {
+
+            static let MaximumHeight: CGFloat = 30
+            static let MaximumWidth: CGFloat = 370
+        }
+    }
+
+    enum Animation {
+
+        static let BaseTime: CGFloat = 0.1
+    }
+}
